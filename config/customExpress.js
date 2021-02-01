68867168cd4e6c82ef9f1c1e0f42176352480b55
moitank <marcelo.moita@al.resilia.com.br>
2021-02-01 15:55:43 -0300

    Primeiro commit

---
+++ config/customExpress.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const consign = require('consign')
+const bodyParser = require('body-parser')
+
+
+module.exports = () => {
+
+    const app = express()
+
+    app.use(bodyParser.json())
+
+    consign()
+        .include('controllers')
+        .into(app)
+    
+    return app
+}
+
---
+++ controllers/atendimentos.js
@@ -0,0 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimetos e está realizando um GET'))
+
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body 
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
+    })
+}
