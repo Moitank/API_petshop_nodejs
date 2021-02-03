29ef1a992b0727af3765dd8ceffa7ed2557c6cea
moitank <marcelo.moita@al.resilia.com.br>
2021-02-03 16:14:01 -0300

    DELETE adicionado a API

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -39,7 +39,7 @@ class Atendimento {
                 if(erro) {
                     res.status(400).json(erro)
             }   else {
-                    res.status(201).json(resultados)
+                    res.status(201).json(atendimento)
             }
         } )
         }
@@ -86,7 +86,19 @@ class Atendimento {
             if(erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json(resultados)
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
